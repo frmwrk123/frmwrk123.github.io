bd7cc13b8ed8e5d61da56f2eba5e0b7c1e0ae373
Jim Martens <github@2martens.de>
2017-12-25 21:05:47 +0100

    Moved deploy script to subdirectory and excluded that from jekyll build
    Signed-off-by: Jim Martens <github@2martens.de>

---
+++ scripts/deploy.sh
@@ -0,0 +1,2 @@
+rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR/_site martens7@wolf.uberspace.de:tmp/$TRAVIS_BUILD_ID
+ssh martens7@wolf.uberspace.de rm -rf tmp/old.build && mkdir tmp/old.build && mv html/* tmp/old.build/ && mv tmp/$TRAVIS_BUILD_ID/* html/
