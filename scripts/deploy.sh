29ebf967f56dc0b7537498b71d9635991440f527
Jim Martens <github@2martens.de>
2017-12-25 21:18:30 +0100

    Fixed deploy script
    Signed-off-by: Jim Martens <github@2martens.de>

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR/_site martens7@wolf.uberspace.de:tmp/$TRAVIS_BUILD_ID
-ssh martens7@wolf.uberspace.de rm -rf tmp/old.build && mkdir tmp/old.build && mv html/* tmp/old.build/ && mv tmp/$TRAVIS_BUILD_ID/* html/
+rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR/_site/* martens7@wolf.uberspace.de:tmp/$TRAVIS_BUILD_ID
+ssh martens7@wolf.uberspace.de rm -rf tmp/old.build
+ssh martens7@wolf.uberspace.de mkdir tmp/old.build
+ssh martens7@wolf.uberspace.de mv html/* tmp/old.build/
+ssh martens7@wolf.uberspace.de mv tmp/$TRAVIS_BUILD_ID/* html/
