580d0186b6d2b0d60238de0907aa5c557ad9e471
Jim Martens <github@2martens.de>
2017-12-29 10:27:06 +0100

    Fixed overlay issues of main and sub menu on small devices
    Signed-off-by: Jim Martens <github@2martens.de>

--- _assets/js/main.js
+++ _assets/js/main.js
@@ -1,3 +1,14 @@
 //= require popper
 //= require bootstrap/util
 //= require bootstrap/collapse
+
+"use strict";
+$(function() {
+    let mainMenu = $('#mainmenuNavbar');
+    mainMenu.on('show.bs.collapse', function (e) {
+        $('#submenuNavbar').addClass('invisible');
+    });
+    mainMenu.on('hidden.bs.collapse', function (e) {
+        $('#submenuNavbar').removeClass('invisible');
+    });
+});
