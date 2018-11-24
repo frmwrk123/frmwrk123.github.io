ba42af74a80955606750435aa585b661481de2b6
Jim Martens <github@2martens.de>
2018-11-24 15:19:51 +0100

    Added back-to-top button
    Signed-off-by: Jim Martens <github@2martens.de>

--- _assets/js/main.js
+++ _assets/js/main.js
@@ -11,4 +11,22 @@ $(function() {
     mainMenu.on('hidden.bs.collapse', function (e) {
         $('#submenuNavbar').removeClass('invisible');
     });
+
+    let backToTop = $('#back-to-top');
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 50) {
+            backToTop.fadeIn();
+        } else {
+            backToTop.fadeOut();
+        }
+    });
+    backToTop.click(function() {
+        backToTop.tooltip('hide');
+        $('body,html').animate({
+            scrollTop: 0
+        }, 800);
+        return false;
+    });
+
+    backToTop.tooltip();
 });
