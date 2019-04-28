32dd6acf3f1121ac40c3da5b843c26fcb40632e7
Jim Martens <github@2martens.de>
2019-04-28 11:07:57 +0200

    Added countdown to website
    Signed-off-by: Jim Martens <github@2martens.de>

--- _assets/javascripts/main.js
+++ _assets/javascripts/main.js
@@ -32,4 +32,19 @@ $(function() {
         throttle: 250,
         unload: false
     });
+
+    let countdownSpan = $("#countdown");
+    countdown.setFormat({singular: " Millisekunde| Sekunde| Minute| Stunde| Tag| Woche| Monat| Jahr| Jahrzehnt| Jahrhundert| Jahrtausend",
+                        plural: " Millisekunden| Sekunden| Minuten| Stunden| Tage| Wochen| Monate| Jahre| Jahrzehnte| Jahrhunderte| Jahrtausende",
+                        last: " und ",
+                        delim: ", "});
+    let counterID = countdown(function(timestamp) {
+            countdownSpan.innerText = timestamp.toLocaleString();
+        }, new Date(2019, 5, 26, 18, 0),
+        countdown.DAYS |
+        countdown.HOURS |
+        countdown.MINUTES |
+        countdown.SECONDS
+    );
+
 });
